d21d884939409cdbfc549d46accac63dbd4448fd
EgoExMachina <pablopezm@gmail.com>
2023-01-31 21:51:17 -0300

    Primer commit

---
+++ js/script.js
@@ -0,0 +1,114 @@
+// Array información regiones
+
+let infoRegiones = [
+    {
+        'id': 01,
+        'nombre': 'Región de Arica y Parinacota',
+        'atractivos': ['Lago Chungará','Momias Chinchorro'],
+        'foto': "img/01.jpg"
+    },
+    {
+        'id': 02,
+        'nombre': 'Región de Tarapacá',
+        'atractivos': ['Salitrera Humberstone','Playas'],
+        'foto': "img/02.jpg"
+    },
+    {
+        'id': 03,
+        'nombre': 'Región de Antofagasta',
+        'atractivos': ['San Pedro de Atacama','Valle de La Luna'],
+        'foto': "img/03.jpg"
+    },
+    {
+        'id': 04,
+        'nombre': 'Región de Atacama',
+        'atractivos': ['Bahía Inglesa','Caldera'],
+        'foto': "img/04.jpg"
+    },
+    {
+        'id': 05,
+        'nombre': 'Región de Coquimbo',
+        'atractivos': ['Valle del Elqui','Cruz del Milenio'],
+        'foto': "img/05.jpg"
+    },
+    {
+        'id': 06,
+        'nombre': 'Región de Valparaíso',
+        'atractivos': ['Reloj de Flores','Ascensores cerros'],
+        'foto': "img/06.jpg"
+    },
+    {
+        'id': 07,
+        'nombre': 'Región Metropolitana',
+        'atractivos': ['Plaza de Armas','Parque Forestal'],
+        'foto': "img/07.jpg"
+    },
+    {
+        'id': 08,
+        'nombre': 'Región de O\'Higgins',
+        'atractivos': ['Sewell','Rancagua no existe'],
+        'foto': "img/08.jpg"
+    },
+    {
+        'id': 09,
+        'nombre': 'Región del Maule',
+        'atractivos': ['Siete Tazas','Completos mojados'],
+        'foto': "img/09.jpg"
+    },
+    {
+        'id': 10,
+        'nombre': 'Región del Bío-Bío',
+        'atractivos': ['Salto del Laja','Dichato'],
+        'foto': "img/10.jpg"
+    },
+    {
+        'id': 11,
+        'nombre': 'Región de La Araucanía',
+        'atractivos': ['PN Conguillío','PN Huerquehue'],
+        'foto': "img/11.jpg"
+    },
+    {
+        'id': 12,
+        'nombre': 'Región de Los Ríos',
+        'atractivos': ['Río Calle-Calle','Fuerte Niebla'],
+        'foto': "img/12.jpg"
+    },
+    {
+        'id': 13,
+        'nombre': 'Región de Los Lagos',
+        'atractivos': ['Lago Llanquihue','Lago Todos los Santos'],
+        'foto': "img/13.jpg"
+    },
+    {
+        'id': 14,
+        'nombre': 'Región de Aysén',
+        'atractivos': ['Laguna San Rafael','Catedrales de Mármol'],
+        'foto': "img/14.jpg"
+    },
+    {
+        'id': 15,
+        'nombre': 'Región de Magallanes',
+        'atractivos': ['Torres del Paine','Fide XII'],
+        'foto': "img/15.jpg"
+    }  
+];
+
+// Función que cambia contenido de Card según hover en área
+
+const card = document.getElementById('tarjeta')
+const imagen = document.getElementById('imagenRegion');
+const titulo = document.getElementById('nombre');
+const atr1 = document.getElementById('atractivo1');
+const atr2 = document.getElementById('atractivo2');
+
+function hover()
+{
+
+
+}
+
+// Crea un array a partir de las areas del mapa
+
+// const regiones = Array.from(document.getElementsByTagName('area'));
+
+
